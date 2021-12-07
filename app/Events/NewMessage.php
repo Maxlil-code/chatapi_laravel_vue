8bf80a2087faeaf07aa31538aae05d45f4f3abad
tolos123 <alemchu123@yahoo.com>
2021-12-07 19:26:00 +0100

    remove quere

--- app/Events/NewMessage.php
+++ app/Events/NewMessage.php
@@ -10,8 +10,9 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 use App\Models\Chat;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 
-class NewMessage implements ShouldBroadcast
+class NewMessage implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
     public $message;
